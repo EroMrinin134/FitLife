e8dd08e4e30212828a08143a6822e6c45e73c4ea
Ero Mrinin <47291495+EroMrinin134@users.noreply.github.com>
2026-05-25 13:48:39 +0400

    ver 2

--- fit_life.py
+++ fit_life.py
@@ -8,80 +8,82 @@ sys.stdout.reconfigure(encoding='utf-8')
 
 # 1. Знакомство
 while True:
-	user_name = input("Введите ваше имя: ")
+    user_name = input("Введите ваше имя: ")
 
-	if len(user_name) == 0:
-		print("Пустое имя недопустимо. Пожалуйста, попробуйте снова.")
-		continue
+    if len(user_name) == 0:
+        print("Пустое имя недопустимо.")
+        continue
 
-	# Конец валидации.
-	break
+    # Конец валидации.
+    break
 
 while True:
-	user_age = input("Введите ваше возраст: ")
+    user_age = input("Введите ваше возраст: ")
 
-	if len(user_age) == 0:
-		print("Возраст не введён. Пожалуйста, попробуйте снова.")
-		continue
+    if len(user_age) == 0:
+        print("Возраст не введён.")
+        continue
 
-	try:
-		user_age = int(user_age)
-	except ValueError:
-		print("У возраста некорретный формат числа. Пожалуйста, попробуйте снова.")
-		continue
+    try:
+        user_age = int(user_age)
+    except ValueError:
+        print("У возраста некорретный формат числа.")
+        continue
 
-	if user_age < 0:
-		print("Возраст не может быть отрицательным числом. Пожалуйста, попробуйте снова.")
-		continue
+    if user_age < 0:
+        print("Возраст не может быть отрицательным числом.")
+        continue
 
-	# Конец валидации.
-	break
+    # Конец валидации.
+    break
 
 # 2. Сбор данных
 while True:
-	user_weight = input("Введите ваш вес (в кг): ")
+    user_weight = input("Введите ваш вес (в кг): ")
 
-	if len(user_weight) == 0:
-		print("Вес не введён. Пожалуйста, попробуйте снова.")
-		continue
+    if len(user_weight) == 0:
+        print("Вес не введён.")
+        continue
 
-	user_weight = user_weight.replace(",", ".") # Запятая допустима, как разделитель.
+    # Запятая допустима, как разделитель.
+    user_weight = user_weight.replace(",", ".")
 
-	try:
-		user_weight = float(user_weight)
-	except ValueError:
-		print("У веса некорретный формат числа. Пожалуйста, попробуйте снова.")
-		continue
+    try:
+        user_weight = float(user_weight)
+    except ValueError:
+        print("У веса некорретный формат числа.")
+        continue
 
-	if user_weight < 0:
-		print("Вес не может быть отрицательным числом. Пожалуйста, попробуйте снова.")
-		continue
+    if user_weight < 0:
+        print("Вес не может быть отрицательным числом.")
+        continue
 
-	# Конец валидации.
-	break
+    # Конец валидации.
+    break
 
 while True:
 
-	user_height = input("Введите ваш рост (в метрах): ")
+    user_height = input("Введите ваш рост (в метрах): ")
 
-	if len(user_height) == 0:
-		print("Рост не введён. Пожалуйста, попробуйте снова.")
-		continue
+    if len(user_height) == 0:
+        print("Рост не введён. Пожалуйста, попробуйте снова.")
+        continue
 
-	user_height = user_height.replace(",", ".") # Запятая допустима, как разделитель.
+    # Запятая допустима, как разделитель.
+    user_height = user_height.replace(",", ".")
 
-	try:
-		user_height = float(user_height)
-	except ValueError:
-		print("У роста некорретный формат числа. Пожалуйста, попробуйте снова.")
-		continue
+    try:
+        user_height = float(user_height)
+    except ValueError:
+        print("У роста некорретный формат числа.")
+        continue
 
-	if user_height < 0:
-		print("Рост не может быть отрицательным числом. Пожалуйста, попробуйте снова.")
-		continue
+    if user_height < 0:
+        print("Рост не может быть отрицательным числом.")
+        continue
 
-	# Конец валидации.
-	break
+    # Конец валидации.
+    break
 
 # 3. Логика расчетов (Функции как "черный ящик": используем арифметику)
 # Формула ИМТ: вес разделить на (рост в квадрате)
@@ -94,7 +96,7 @@ water_ml = user_weight * WATER_PER_KG
 water_l = water_ml / 1000
 
 # 4. Вывод красивого результата
-print(f"Отчет для пользователя: {user_name} ({user_age} г.)!", flush=True)
-print(f"Ваш индекс массы тела: {bmi}", flush=True)
-print(f"Рекомендуемая норма воды: {water_l} л. в день", flush=True)
-print("Расчет окончен. Будьте здоровы!", flush=True)
+print(f"Отчет для пользователя: {user_name} ({user_age} г.)!")
+print(f"Ваш индекс массы тела: {bmi}")
+print(f"Рекомендуемая норма воды: {water_l} л. в день")
+print("Расчет окончен. Будьте здоровы!")
